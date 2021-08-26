a52c038b2a91fbd1b7b5fe18aeb2b9c63fee5ad1
Joelinqi <89511825+Joelinqi@users.noreply.github.com>
2021-08-26 20:57:14 +0800

    Update datadb.h
    建立数据库对象的操作，里面封装了插入、查询、匹配的操作，打开数据库的操作在构造函数中，关闭数据库的操作在析构函数中

--- Tcp_Server/datadb.h
+++ Tcp_Server/datadb.h
@@ -1,11 +1,27 @@
-#ifndef DATADB_H
-#define DATADB_H
+#ifndef DB_H
+#define DB_H
+#include"user_info.h"
+#include"QSqlDatabase"
+#include"QString"
 
-
-class DataDB
+class db
 {
+private:
+    db();
+    QSqlDatabase db_;
+    ~db();
 public:
-    DataDB();
+    //静态获取数据库对象
+    static db *getDatabaseIntence();
+    //把用户信息插入数据库,ture为插入成功，false为插入失败
+    bool insertSql(user_info &user);
+    //通过用户名在数据库查找，返回true为查找到，用户名已经存在，false为不存在
+    bool selectSql(QString username);
+    //判断用户名密码是否存在，是否匹配
+    //true为匹配成功，密码正确，false为匹配失败，密码错误
+    bool loginJudge(QString username,QString password);
 };
 
-#endif // DATADB_H
+#endif // DB_H
+
+
