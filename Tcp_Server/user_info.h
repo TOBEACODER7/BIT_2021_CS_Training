c19ebea4f87d41c022a9ee2b9adc2a05ce730b6c
Joelinqi <89511825+Joelinqi@users.noreply.github.com>
2021-08-27 20:06:22 +0800

    Update user_info.h

--- Tcp_Server/user_info.h
+++ Tcp_Server/user_info.h
@@ -4,20 +4,25 @@
 #include"QDebug"
 
 //建立用户信息的对象，用于储存用户信息
-//分别为：用户名，密码，姓名，电话号码，电子邮箱
-//其中，用户名和密码不能为空，其他可以为空，默认值为“unknown”
+//分别为：序号,用户名，密码，姓名，电话号码，电子邮箱
+//其中，序号，用户名和密码不能为空，其他可以为空，默认值为“unknown”
 //public中的方法为获取信息的途径
 
 class user_info
 {
 public:
-    user_info(QString,QString,QString,QString,QString);
+    user_info(int,QString,QString,QString,QString,QString);
+    user_info(int,QString,QString,QString,QString);
+    user_info(int,QString,QString,QString);
+    user_info(int,QString,QString);
     QString getUsername();
     QString getPassword();
     QString getName();
     QString getPhone();
     QString getemail();
+    int     getUno();
 private:
+    int uno;
     QString username;
     QString password;
     QString name;
