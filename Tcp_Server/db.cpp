914d2feed3dc32fe47029d3e8479896dff5d6b1d
Joelinqi <89511825+Joelinqi@users.noreply.github.com>
2021-08-27 20:04:22 +0800

    Update db.cpp

--- Tcp_Server/db.cpp
+++ Tcp_Server/db.cpp
@@ -3,59 +3,109 @@
 #include"QDebug"
 #include <QSql>
 #include <QSqlDatabase>
-#include <QDebug>
 #include <QSqlError>
 #include <QSqlQuery>
 #include <QSqlRecord>
+#include <QSqlQueryModel>
 #include "db.h"
+
 
 db::db()
 {
-    //创建数据库数据库
-    QSqlDatabase db_=QSqlDatabase::addDatabase("QMYSQL");
-    db_.setHostName("127.0.0.1");
-    db_.setDatabaseName("user_info");
-    db_.setPort(3306);
-    db_.setUserName("root");
-    db_.setPassword("ly1426847100!");
-    //打开数据库
-    if(!db_.open())
-    {
-         qDebug()<<"Database open fail!Because:"<<db_.lastError().text();
-    }
-
+   qDebug()<<"数据库对象建立!";
 }
 
 db::~db()
 {
-    db_.close();
+   qDebug()<<"数据库对象释放!";
 }
 
-db * db::getDatabaseIntence(){
-    static db mysql;
-    return &mysql;
+
+
+int db::getNum(){
+
+    QSqlDatabase db=QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("127.0.0.1");
+    db.setDatabaseName("？？？");
+    db.setPort(3306);
+    db.setUserName("root");
+    db.setPassword("？？？");
+    if(!db.open())
+    {
+         qDebug()<<"数据库在函数getNum打开失败!原因是:"<<db.lastError().text();
+    }
+    db.close();
+
+
+    QSqlQuery query(db);
+    query.exec ( "SELECT * FROM user_info");
+    QSqlQueryModel *queryModel = new QSqlQueryModel();
+    queryModel->setQuery(query);
+    int nRecordcount = queryModel->rowCount();
+    return nRecordcount;
 }
-//数据库表名为userinfo，里面有username，password，name，phone，email四个char类型的表，建议都设置成非空
+
 bool db::insertSql(user_info &user){
-    QString sql=QString("INSERT INTO user_info VALUES('1%','2%','3%','4%','5%')")
-            .arg(user.getUsername())
-            .arg(user.getPassword())
+
+
+    QSqlDatabase db=QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("127.0.0.1");
+    db.setDatabaseName("？？？");
+    db.setPort(3306);
+    db.setUserName("root");
+    db.setPassword("？？？");
+    if(!db.open())
+    {
+         qDebug()<<"数据库在函数 insertSql 打开失败!原因是:"<<db.lastError().text();
+    }
+
+
+    QString sql1=QString("INSERT INTO user_info VALUES('1%','2%','3%','4%');")
+            .arg(user.getUno())
             .arg(user.getName())
             .arg(user.getPhone())
             .arg(user.getemail());
-    QSqlQuery query(db_);
-    if(!query.exec(sql)){
-        qDebug()<<"Insert fail!Because:"<< query.lastError().text();
+    QString sql2=QString("INSERT INTO login_info VALUES('1%','2%','3%');")
+            .arg(user.getUno())
+            .arg(user.getUsername())
+            .arg(user.getPassword());
+
+    QSqlQuery query(db);
+    if(!query.exec(sql1)){
+        qDebug()<<"插入失败!原因是:"<< query.lastError().text();
+        db.close();
         return false;
     }
-    qDebug()<<"Insert success!";
+    if(!query.exec(sql2)){
+        qDebug()<<"插入失败!原因是:"<< query.lastError().text();
+        db.close();
+        return false;
+    }
+    qDebug()<<"插入成功!";
+    db.close();
     return true;
 }
 
 bool db::selectSql(QString username)
 {
-    QString sql = QString("select username from user_info where name = :name;");
-    QSqlQuery query(db_);
+
+    QSqlDatabase db=QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("127.0.0.1");
+    db.setDatabaseName("？？？");
+    db.setPort(3306);
+    db.setUserName("root");
+    db.setPassword("？？？");
+
+
+    //打开数据库
+    if(!db.open())
+    {
+         qDebug()<<"数据库在函数 selectSql中打开失败!原因是:"<<db.lastError().text();
+    }
+
+
+    QString sql = QString("select username from login_info where username = :name;");
+    QSqlQuery query(db);
     query.prepare(sql);
     query.bindValue(":name",QVariant(username));
     query.exec();
@@ -63,29 +113,50 @@ bool db::selectSql(QString username)
     if(query.next())
     {
         //匹配成功
-        if(username==query.value(0).toString()){
+        qDebug()<<query.value(1).toString();
+        if(username==query.value(1).toString()){
+            db.close();
             return true;
         }
     }
     //匹配失败
+    db.close();
     return false;
 }
 
 bool db::loginJudge(QString username, QString password)
 {
-    QString sql = QString("select username,password from user_info where name = :name;");
-    QSqlQuery query(db_);
+
+    QSqlDatabase db=QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("127.0.0.1");
+    db.setDatabaseName("???");
+    db.setPort(3306);
+    db.setUserName("root");
+    db.setPassword("???");
+
+
+    if(!db.open())
+    {
+         qDebug()<<"数据库在函数loginJudge打开失败！原因是："<<db.lastError().text();
+    }
+
+
+    QString sql = QString("select username,password from login_info where username = :name;");
+    QSqlQuery query(db);
     query.prepare(sql);
     query.bindValue(":name",QVariant(username));
     query.exec();
     //查询结束
     if(query.next())
     {
-        if(password==query.value(1).toString()){
+        qDebug()<<query.value(2).toString();
+        if(password==query.value(2).toString()){
             //密码正确
+            db.close();
             return true;
         }
     }
     //密码错误
+    db.close();
     return false;
 }
