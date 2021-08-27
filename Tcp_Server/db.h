d893d1a4d696c23098fbaf548408e1326c4d0bf0
Joelinqi <89511825+Joelinqi@users.noreply.github.com>
2021-08-27 20:05:38 +0800

    Update db.h

--- Tcp_Server/db.h
+++ Tcp_Server/db.h
@@ -3,23 +3,22 @@
 #include"user_info.h"
 #include"QSqlDatabase"
 #include"QString"
+#include <QSqlQueryModel>
 
 class db
 {
 private:
-    db();
-    QSqlDatabase db_;
-    ~db();
 public:
-    //静态获取数据库对象
-    static db *getDatabaseIntence();
+    db();
+    ~db();
     //把用户信息插入数据库,ture为插入成功，false为插入失败
     bool insertSql(user_info &user);
     //通过用户名在数据库查找，返回true为查找到，用户名已经存在，false为不存在
     bool selectSql(QString username);
     //判断用户名密码是否存在，是否匹配
-    //true为匹配成功，密码正确，false为匹配失败，密码错误
     bool loginJudge(QString username,QString password);
+    //获取记录个数
+    int  getNum();
 };
 
 #endif // DB_H
