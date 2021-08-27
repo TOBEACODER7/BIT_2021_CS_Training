d8b08e3587f9f1a9b02bb66b15bf3a1f6f1b5302
Joelinqi <89511825+Joelinqi@users.noreply.github.com>
2021-08-27 20:08:34 +0800

    Update user_info.cpp

--- Tcp_Server/user_info.cpp
+++ Tcp_Server/user_info.cpp
@@ -1,10 +1,12 @@
 #include "user_info.h"
 
-user_info::user_info(QString username_,
+user_info::user_info(int uno_,
+                     QString username_,
                      QString password_,
-                     QString name_="unkown",
-                     QString phone_="unkown",
-                     QString email_="unkown"):
+                     QString name_,
+                     QString phone_,
+                     QString email_):
+                     uno(uno_),
                      username(username_),
                      password(password_),
                      name(name_),
@@ -12,7 +14,55 @@ user_info::user_info(QString username_,
                      email(email_)
 
 {
-        qDebug()<<"user_info build success！";
+        qDebug()<<"user_info 对象创建成功！,已知个数=6";
+}
+
+user_info::user_info(int uno_,
+                     QString username_,
+                     QString password_):
+                     uno(uno_),
+                     username(username_),
+                     password(password_)
+
+{
+    name="unkown";
+    phone="unkown";
+    email="unkown";
+    qDebug()<<"user_info 对象创建成功！,已知个数=3";
+}
+
+user_info::user_info(int uno_,
+                     QString username_,
+                     QString password_,
+                     QString name_):
+                     uno(uno_),
+                     username(username_),
+                     password(password_),
+                     name(name_)
+{
+    phone="unkown";
+    email="unkown";
+    qDebug()<<"user_info 对象创建成功！,已知个数=4";
+}
+
+user_info::user_info(int uno_,
+                     QString username_,
+                     QString password_,
+                     QString name_,
+                     QString phone_):
+                     uno(uno_),
+                     username(username_),
+                     password(password_),
+                     name(name_),
+                     phone(phone_)
+
+{
+    email="unkown";
+    qDebug()<<"user_info 对象创建成功！,已知个数=5";
+}
+
+int user_info::getUno(){
+    return uno;
 }
 QString user_info::getUsername(){
     return  username;
